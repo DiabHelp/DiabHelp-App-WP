9971255a432cceafb8783b0165b46c2b037c9323
egea-a <antoine.egea@epitech.eu>
2016-10-06 13:34:06 +0200

    Connection DB OK, linke avec l'autocloplete

---
+++ Diabhelp/Modules/Glucocompteur/Aliment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diabhelp.Modules.Glucocompteur
+{
+    public class Aliment
+    {
+        public String alimentname { get; set; }
+        public Double weight { get; set; }
+        public Double Glucides { get; set; } // g/100g
+        public Double totalGlucids { get; set; }
+        public Uri alimentLogo { get; set; }
+
+        public Aliment() { }
+
+        public Aliment(String name, Double weight, Double glucids)
+        {
+            this.alimentname = name;
+            this.weight = weight;
+            this.Glucides = glucids;
+            this.totalGlucids = (weight * glucids) / 100;
+        }
+
+
+    }
+}
--- Diabhelp/Modules/Glucocompteur/GlucocompteurAlimentScreen.xaml.cs
+++ Diabhelp/Modules/Glucocompteur/GlucocompteurAlimentScreen.xaml.cs
@@ -1,17 +1,12 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,9 +17,81 @@ namespace Diabhelp.Modules.Glucocompteur
     /// </summary>
     public sealed partial class GlucocompteurAlimentScreen : Page
     {
+        List<String> fakeDB = new List<string>() { "Alim1", "Alim2", "Alim3", "Alim4", "Alim5", "Alim6" };
+        SQLiteConnection db;
+        public String fieldObjectName = "alimentname";
+        public String fieldObjectGlucides = "Glucides";
+        public String tableName = "aliments_search";
+        public List<Aliment> alimlist = new List<Aliment>();
+
+        public Aliment selectedAliment { get; private set; }
+
+        private async void loadAlimentsDB()
+        {
+            string DBFile = @"Assets\aliments.db";
+            StorageFolder InstallationFolder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+            StorageFile file = await InstallationFolder.GetFileAsync(DBFile);
+            db = new SQLiteConnection(file.Path);
+
+        }
+
         public GlucocompteurAlimentScreen()
         {
             this.InitializeComponent();
+            Debug.WriteLine("Open file blabla");
+            string DBPath = @"Assets\aliments.db";
+            FileStream fs = File.OpenRead(DBPath);
+            if (fs == null)
+            {
+                Debug.WriteLine("Rate openfile");
+                return;
+            }
+            loadAlimentsDB();
+            alimentInput.ItemsSource = alimlist;
+        }
+
+        private void addAlimentBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Adding aliment : " + alimentInput.Text);
+        }
+
+        private List<Aliment> getAlimListFromDB(String input)
+        {
+            Debug.WriteLine("Try getAlimList for input: " + input);
+
+            string sql = "";
+            input= input.Replace("'", "''").Replace("\"", "\"\"");
+            sql += "SELECT alimentname, Glucides FROM " + tableName;
+            sql += " WHERE " + fieldObjectName + " LIKE '%" + input + "%'";
+            sql += " ORDER BY " + fieldObjectName + "='" + input + "' DESC,";
+            sql += fieldObjectName + " LIKE '" + input + "%' DESC,";
+            sql += fieldObjectName + " LIKE '%" + input + "%' DESC";
+            sql += " LIMIT 8";
+
+            List<Aliment> newalimlist = db.Query<Aliment>(sql);
+            foreach (Aliment alim in newalimlist)
+            {
+                Debug.WriteLine("Name : " + alim.alimentname + ", glucides : " + alim.Glucides);
+            }
+            return newalimlist;
+        }
+
+        private void alimentInput_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            if (args.Reason == AutoSuggestionBoxTextChangeReason.UserInput)
+            {
+                //sender.ItemsSource = fakeDB.Where(s => s.Contains(sender.Text));
+                alimlist = getAlimListFromDB(sender.Text);
+                sender.ItemsSource = alimlist;
+            }
+
+        }
+
+        private void alimentInput_SuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
+        {
+            selectedAliment = (Aliment)args.SelectedItem;
+            sender.Text = selectedAliment.alimentname;
+            Debug.WriteLine("Selected aliment : " + selectedAliment.alimentname + " glucides = " + selectedAliment.Glucides);
         }
     }
 }
